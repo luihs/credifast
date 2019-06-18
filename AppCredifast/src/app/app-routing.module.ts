d7b24a0b5bdea41607de3fd1a4adbd35f080573e
Luis <luis.riverahuaman@hotmail.com>
2019-06-18 00:54:17 -0500

    modulo obtencion datos reniec

--- AppCredifast/src/app/app-routing.module.ts
+++ AppCredifast/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { InicioVisitadorComponent } from './inicio-visitador/inicio-visitador.component';
+import { RegistrarVisitaComponent } from './registrar-visita/registrar-visita.component';
 
 const routes: Routes = [
-  { path: "homevisitador", component: InicioVisitadorComponent}
+  { path: "homevisitador", component: InicioVisitadorComponent },
+  { path: "registrarvisita", component: RegistrarVisitaComponent }
 ];
 
 @NgModule({
